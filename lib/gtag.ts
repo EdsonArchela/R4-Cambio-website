bcdd8016ebf4d883b2938a1b5c77cb623b9d150a
Edson Archela <edson.archela@r4cambio.com.br>
2021-05-04 13:31:25 -0300

    .6

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -2,6 +2,7 @@ export const GA_TRACKING_ID = 'G-8DDBY6L2L3'
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL): void => {
+  /* tslint:disable */
   window.gtag('config', GA_TRACKING_ID, {
     page_path: url,
   })
