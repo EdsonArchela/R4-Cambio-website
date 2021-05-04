7576a753d7526aa69f9b0f29a05cbf007b4f9117
Edson Archela <edson.archela@r4cambio.com.br>
2021-05-04 12:54:58 -0300

    .3

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -32,19 +32,7 @@ export default class MyDocument extends Document {
         ),
         head: [
           <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />,
-          <script
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{
-              __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-          `,
-            }}
-          />,
+          <script src="/gs.js" async />,
         ],
       }
     } finally {
