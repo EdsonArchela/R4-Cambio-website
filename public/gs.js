a3fe45f028e1308665f8bb493975dd4d1461640c
Edson Archela <edson.archela@r4cambio.com.br>
2021-05-04 13:25:01 -0300

    .5

--- public/gs.js
+++
@@ -1,8 +0,0 @@
-var dataLayer = window.dataLayer || []
-function gtag() {
-  dataLayer.push(arguments)
-}
-gtag('js', new Date())
-gtag('config', '${GA_TRACKING_ID}', {
-  page_path: window.location.pathname,
-})
